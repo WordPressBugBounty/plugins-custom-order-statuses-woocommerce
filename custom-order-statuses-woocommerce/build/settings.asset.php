b357977e678edcf93a2a5c90cff4ad0765de0749
Sean Murphy <sean@iamseanmurphy.com>
2026-06-30 07:37:50 +0000

    3.0.0

---
+++ custom-order-statuses-woocommerce/build/settings.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'b5f2393dc5f9f63d6a88');
